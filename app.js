07098a311a74398e30c5fa05b359310fc3f14b17
chasingSublimity <blakemsager@gmail.com>
2016-10-25 09:44:18 -0500

    add delete and add item functionality

--- app.js
+++ app.js
@@ -5,8 +5,26 @@ $(document).ready(function() {
 	})
 	.on('click', '.shopping-item-delete', function(event) {
 		console.log($(event.target));
+		$(event.target).closest("li").remove("li");
+	});
+	$("#js-shopping-list-form").submit(function (event) {
+		event.preventDefault();
 
-
-	})
-
+		var item = $("#shopping-list-entry").val();
+		var liTemplate = '\
+		<li>\
+            <span class="shopping-item">' + item + '</span>\
+            <div class="shopping-item-controls">\
+            <button class="shopping-item-toggle">\
+                <span class="button-label">check</span>\
+            </button>\
+            <button class="shopping-item-delete">\
+                <span class="button-label">delete</span>\
+            </button>\
+            </div>\
+        </li>';
+        $(".shopping-list").append(liTemplate);
+        $("#shopping-list-entry").val("");
+        $("#shopping-list-entry").focus();
+	});
 });
